317bb69693191c4ece589f11c7178bfd3327a69d
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-04-05 01:54:02 +0400

    add contacts modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
+import ContactsModal from './components/ContactsModal'
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    return (
+        <ContactsModal />
+    )
+   
 }
 
 export default App
