522ec30dbd3b8021a7a7aad8677071306ccdcbf5
Valerio Capogna <valerio.capogna@fornacestudio.com>
2024-04-04 11:31:36 +0200

    login page created

--- frontend/app/(auth)/authentication/sign-in/page.js
+++ frontend/app/(auth)/authentication/sign-in/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 // import node module libraries
-import { Row, Col, Card, Form, Button, Image } from 'react-bootstrap';
+import { Row, Col, Card, Form, Button } from 'react-bootstrap';
 import Link from 'next/link';
 
 // import hooks
@@ -9,6 +9,7 @@ import useMounted from 'hooks/useMounted';
 
 const SignIn = () => {
   const hasMounted = useMounted();
+  
   return (
     <Row className="align-items-center justify-content-center g-0 min-vh-100">
       <Col xxl={4} lg={6} md={8} xs={12} className="py-8 py-xl-0">
@@ -17,48 +18,37 @@ const SignIn = () => {
           {/* Card body */}
           <Card.Body className="p-6">
             <div className="mb-4">
-              <Link href="/"><Image src="/images/brand/logo/logo-primary.svg" className="mb-2" alt="" /></Link>
+              <h1>
+                <Link href="/">TechSolutions Inc</Link>
+              </h1>
               <p className="mb-6">Please enter your user information.</p>
             </div>
             {/* Form */}
             {hasMounted &&
               <Form>
-                {/* Username */}
+                {/* Email */}
                 <Form.Group className="mb-3" controlId="username">
-                  <Form.Label>Username or email</Form.Label>
-                  <Form.Control type="email" name="username" placeholder="Enter address here" required="" />
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" name="username" placeholder="Enter address here" required />
                 </Form.Group>
 
                 {/* Password */}
                 <Form.Group className="mb-3" controlId="password">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" name="password" placeholder="**************" required="" />
+                  <Form.Control type="password" name="password" placeholder="**************" required />
                 </Form.Group>
-
-                {/* Checkbox */}
-                <div className="d-lg-flex justify-content-between align-items-center mb-4">
-                  <Form.Check type="checkbox" id="rememberme">
-                    <Form.Check.Input type="checkbox" />
-                    <Form.Check.Label>Remember me</Form.Check.Label>
-                  </Form.Check>
-                </div>
-                <div>
+                
+                <div className="d-md-flex flex-column justify-content-center">
                   {/* Button */}
-                  <div className="d-grid">
-                    <Button variant="primary" type="submit">Sign In</Button>
-                  </div>
-                  <div className="d-md-flex justify-content-between mt-4">
-                    <div className="mb-2 mb-md-0">
-                      <Link href="/authentication/sign-up" className="fs-5">Create An Account </Link>
-                    </div>
-                    <div>
-                      <Link href="/authentication/forget-password" className="text-inherit fs-5">Forgot your password?</Link>
-                    </div>
+                  <Button variant="primary" type="submit">Sign In</Button>
+                  <div className="mt-4 text-center">                    
+                    <Link href="/authentication/forget-password" className="text-inherit fs-5">
+                      <small>Forgot your password?</small>
+                    </Link>                    
                   </div>
                 </div>
-              </Form>}
-
-
+              </Form>
+            }
           </Card.Body>
         </Card>
       </Col>
