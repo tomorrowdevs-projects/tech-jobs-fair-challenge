77c1bb938711cd353bd3864efdf77c0bfcd990e9
Marco Marchionni <marco.marchionni@gmail.com>
2024-04-04 23:27:38 +0200

    Aggiunto menu utente nell'header

--- client/phonebook-app/src/components/header.jsx
+++ client/phonebook-app/src/components/header.jsx
@@ -1,13 +1,45 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Dropdown } from "react-bootstrap";
+import { useUser } from "../user-management/useUser";
+import { useNavigate } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Header = () => {
+  const { user, setUser } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setUser(null);
+    navigate("/");
+  };
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="#" onClick={() => navigate("/contacts")}>
           <strong>TechSolutions Inc.</strong> Phonebook
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          {user && ( // Only display if user is logged in
+            <Dropdown>
+              <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                <i className="bi bi-person-circle"></i>
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu align="end">
+                {user.role === "admin" && (
+                  <Dropdown.Item onClick={() => navigate("/signup")}>
+                    Add User
+                  </Dropdown.Item>
+                )}
+                <Dropdown.Item href="#action/3.3" onClick={handleLogout}>
+                  Sign Out
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
