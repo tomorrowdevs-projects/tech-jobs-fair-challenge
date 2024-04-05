904323f5589ab31fc78de0b141bbf35828e74f4a
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-04-05 19:22:01 +0400

    Added user's profile component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import ContactsModal from './components/ContactsModal'
+import UserProfile from './components/UserProfile'
+
 function App() {
     return (
-        <ContactsModal />
+        <div>
+            <ContactsModal />
+            <UserProfile />
+        </div>
     )
-   
 }
 
 export default App
