1beab0f2f9cf75001fcbc719ab00fa39143dcfdb
FranzAmoroso <amoroso.franz1@gmail.com>
2024-04-05 13:37:50 +0200

    Add Page Login

---
+++ Login/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Login from './components/login.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+)
