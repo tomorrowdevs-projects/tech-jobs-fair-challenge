cf2a1b8144c5a5664816780b9381155182c85329
Stefania <126720391+Aoblu87@users.noreply.github.com>
2024-04-08 14:11:56 +0200

    delete context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from "./components/Dashboard/Dashboard"
-import Login from "./components/login/login"
 import ProfilePage from "./components/ProfilePage/ProfilePage"
-import { ContactModalProvider } from "./context/contactModalContext"
+import Login from "./components/login/login"
 function App() {
     return (
         <>
-            <ContactModalProvider>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/profile" element={<ProfilePage />} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/profile" element={<ProfilePage />} />
 
-                        <Route path="/login" element={<Login />} />
-                    </Routes>
-                </BrowserRouter>
-            </ContactModalProvider>
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
